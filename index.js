5c85cc2fba787091985f8cbabdfb1c426324f1b8
vikram singh <chavikram@gmail.com>
2021-07-05 04:53:16 -0700

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.module.css'
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import $ from 'jquery';
+
 class Todo extends React.Component {
     constructor(props) {
         super(props)
     }
     render() {
-        const styleName = (this.props.item.completed ? "checked" : "");
+        const styleName = (this.props.item.completed ? " list-group-item d-flex justify-content-between align-items-center del-text" : "list-group-item d-flex justify-content-between align-items-center");
         console.log('render called');
         return (
-            <li checked={this.props.item.completed ? 'checked':''} title="xx" className={styleName} onClick={(e) => this.props.markItem(this.props.item.id,e)}>
-                {this.props.item.text}
-                <span className="close" onClick={(e)=>this.props.deleteItem(this.props.item.id,e)}>x</span>
-
-                </li>
+            <a title="xx" className={styleName} onClick={(e) => this.props.markItem(this.props.item.id, e)}>
+                <span style={{ cursor: "pointer" }} className={this.props.item.completed ? "del-text" : ""}>
+                    {this.props.item.text}
+                </span>
+                <span style={{ cursor: "pointer" }} className="badge badge-primary badge-pill" >
+                    <FontAwesomeIcon icon={faEdit} onClick={(e) => this.props.editItem(this.props.item.id, e)} /> &nbsp;
+                    <FontAwesomeIcon icon={faTrash} onClick={(e) => this.props.deleteItem(this.props.item.id, e)} />
+                </span>
+            </a>
         )
     }
 }
@@ -24,26 +34,58 @@ class List extends React.Component {
         super(props)
         this.state = {
             list: [
-               
+
             ]
         }
 
         this.addItem = this.addItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
         this.markItem = this.markItem.bind(this);
+        this.editItem = this.editItem.bind(this);
+
     }
-    markItem(id,e){
+
+    editItem(id, e) {
         e.preventDefault()
         const elementsIndex = this.state.list.findIndex(element => element.id == id)
         let newArray = [...this.state.list]
-        newArray[elementsIndex] = {...newArray[elementsIndex], completed: !newArray[elementsIndex].completed}
+        if (newArray[elementsIndex].completed) {
+            alert('Activity is completed');
+            e.stopPropagation();
+            return;
+        }
+        $('#myInput').val(newArray[elementsIndex].text);
+        $('#myInput').attr("data-id", id);
+        $('#addBtn').text("Update");
+        e.stopPropagation();
+    }
+
+    updateItem(id, e) {
+        e.preventDefault()
+        const elementsIndex = this.state.list.findIndex(element => element.id == id)
+        let newArray = [...this.state.list]
+
+        newArray[elementsIndex] = { ...newArray[elementsIndex], text: $("#myInput").val() }
 
         this.setState({
             list: newArray,
-            });
+        });
+        $("#addBtn").text("Add");
+        $("#myInput").val("");
+        e.stopPropagation();
     }
-    deleteItem(id,e) {
-        
+    markItem(id, e) {
+        e.preventDefault()
+        const elementsIndex = this.state.list.findIndex(element => element.id == id)
+        let newArray = [...this.state.list]
+        newArray[elementsIndex] = { ...newArray[elementsIndex], completed: !newArray[elementsIndex].completed }
+
+        this.setState({
+            list: newArray,
+        });
+    }
+    deleteItem(id, e) {
+
         console.log('delete item called')
         let newArray = [...this.state.list]
         const elementsIndex = this.state.list.findIndex(element => element.id == id)
@@ -54,41 +96,19 @@ class List extends React.Component {
 
         this.setState({
             list: newArray
-        })
-        e.stopPropagation()
+        });
+        e.stopPropagation();
+        alert('item deleted successfully')
     }
     addItem(e) {
-        if(e.type=='click' && e.target.title == 'addBtn'){
-            let inputElement = document.getElementById('myInput');
-            console.log(inputElement.value);
-            var val = inputElement.value;
-            let newId = 0;
-            if (this.state.list.length != 0)
+        if ((e.type == 'click' && e.target.title == 'addBtn') && $('#addBtn').text() == "Update") {
 
-                newId = this.state.list.slice(-1)[0].id + 1;
-            else
-                newId = 1
-            let obj = this.state.list.find((e) => e.text == val)
-            console.log(obj);
-            console.log(val != '');
-
-            if (val != '' && (obj == null || obj.id === undefined)) {
-                this.setState((prevState) => {
-                    return {
-                        list: prevState.list.concat({ id: newId, text: val, completed: false })
-                    }
-                });
-            } else {
-                alert("item already exist or empty");
-            }
-            // list.push(e.currentTarget.value);
-            inputElement.value = ''
-            console.log("length-" + this.state.list.length);
+            this.updateItem($('#myInput').attr("data-id"), e);
+            return;
         }
-        if (e.keyCode == 13) {
-            let inputElement = document.getElementById('myInput');
-            console.log(inputElement.value);
-            var val = inputElement.value;
+        if ((e.type == 'keydown' && e.keyCode == 13) ||
+            (e.type == 'click' && e.target.title == 'addBtn')) {
+            var val = $('#myInput').val();
             let newId = 0;
             if (this.state.list.length != 0)
 
@@ -108,28 +128,32 @@ class List extends React.Component {
             } else {
                 alert("item already exist or empty");
             }
-            // list.push(e.currentTarget.value);
-            inputElement.value = ''
-            console.log("length-" + this.state.list.length);
+            $('#myInput').val('');
 
         }
 
     }
-    showItem(e){
+    showItem(e) {
         alert('clicked');
     }
     render() {
         return (
-            <div>
-                <div id="myDIV" className="header">
-                    <h2>My To Do List</h2>
-                    <input title="todoinput" id="myInput" length="15" placeholder="Title..." onKeyDown={this.addItem} />
-                    <span onClick={this.addItem} className="addBtn" title="addBtn">Add</span>
+            <div className="row">
+                <div className="col-md-4 offset-md-3">
+                    <h2>My Todo List</h2>
+
+
+                    <input type="text" data-id="" title="todoinput" id="myInput" className="form-control" length="15" placeholder="Title..." onKeyDown={this.addItem} />
+
+                    <span id="addBtn" style={{ marginTop: "10px" }} onClick={this.addItem} className="form-control btn btn-primary" title="addBtn">Add</span>
+
+
+                    <br />
+                    <div className="list-group" style={{ marginTop: "10px" }}>
+                        {this.state.list.map((todo) => <Todo editItem={this.editItem} markItem={this.markItem} deleteItem={this.deleteItem} key={todo.id} item={todo}></Todo>)}
+                    </div>
                 </div>
-                <ul>
-                    {this.state.list.map((todo) => <Todo markItem={this.markItem} deleteItem={this.deleteItem} key={todo.id} item={todo}></Todo>)}
-                </ul>
-                </div>
+            </div>
         )
     }
 }
